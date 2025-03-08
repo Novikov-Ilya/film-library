87dbda878be6c51cec51ea1cc1e0b65d638c7522
ilya <mrsmile1988@gmail.com>
2025-03-08 22:38:04 +0300

    fixed gesture issue

--- src/components/MovieCard/CoctailCard.jsx
+++ src/components/MovieCard/CoctailCard.jsx
@@ -28,10 +28,16 @@ export default function CoctailCard({ id, show, close, switchCocktail }) {
 
   const handleTouchEnd = (e) => {
     endTouchX = Math.round(e.changedTouches[0].pageX);
-    if(startTouchX - endTouchX < 0) {
-      directionSwitch = 'prev'
-    } else {
-      directionSwitch = 'next'
+    if (startTouchX - endTouchX < 0) {
+      directionSwitch = 'prev';
+      startTouchX = null;
+      endTouchX = null;
+    }
+
+    if (startTouchX - endTouchX > 0) {
+      directionSwitch = 'next';
+      startTouchX = null;
+      endTouchX = null;
     }
     switchCocktail(directionSwitch)
   }
@@ -53,39 +59,39 @@ export default function CoctailCard({ id, show, close, switchCocktail }) {
 
         </header>
         <hr />
-          <section className="coctail__top-info">
-            <div className="coctail__image">
-              <img src={currentCoctail.strDrinkThumb}
-                alt={currentCoctail.strDrink} />
-            </div>
-            <div className="coctail__attr">
-              <p><b>Title:</b> {currentCoctail.strDrink}</p>
-              <p><b>Category:</b> {currentCoctail.strCategory}</p>
-              <p><b>IBA:</b> {currentCoctail.strIBA || 'Unspecified'}</p>
-              <p><b>Glass:</b> {currentCoctail.strGlass}</p>
-              <h4>Ingridients:</h4>
-              {currentCoctail.strIngredient1 && <p><b>1:</b> {currentCoctail.strIngredient1}</p>}
-              {currentCoctail.strIngredient2 && <p><b>2:</b> {currentCoctail.strIngredient2}</p>}
-              {currentCoctail.strIngredient3 && <p><b>3:</b> {currentCoctail.strIngredient3}</p>}
-              {currentCoctail.strIngredient4 && <p><b>4:</b> {currentCoctail.strIngredient4}</p>}
-              {currentCoctail.strIngredient5 && <p><b>5:</b> {currentCoctail.strIngredient5}</p>}
-              {currentCoctail.strIngredient6 && <p><b>6:</b> {currentCoctail.strIngredient6}</p>}
-              {currentCoctail.strIngredient7 && <p><b>7:</b> {currentCoctail.strIngredient7}</p>}
-              {currentCoctail.strIngredient8 && <p><b>8:</b> {currentCoctail.strIngredient8}</p>}
-              {currentCoctail.strIngredient9 && <p><b>9:</b> {currentCoctail.strIngredient9}</p>}
-              {currentCoctail.strIngredient10 && <p><b>10:</b> {currentCoctail.strIngredient10}</p>}
-            </div>
-          </section>
-          <hr />
-          <section className="coctail__description">
-            <h4>Instructions:</h4>
-            <p>{currentCoctail.strInstructions}</p>
-          </section>
-          <div className="modal__controls">
-            <Button onClick={() => switchCocktail('prev')}>Previous</Button>
-
-            <Button onClick={() => switchCocktail('next')}>Next</Button>
+        <section className="coctail__top-info">
+          <div className="coctail__image">
+            <img src={currentCoctail.strDrinkThumb}
+              alt={currentCoctail.strDrink} />
           </div>
+          <div className="coctail__attr">
+            <p><b>Title:</b> {currentCoctail.strDrink}</p>
+            <p><b>Category:</b> {currentCoctail.strCategory}</p>
+            <p><b>IBA:</b> {currentCoctail.strIBA || 'Unspecified'}</p>
+            <p><b>Glass:</b> {currentCoctail.strGlass}</p>
+            <h4>Ingridients:</h4>
+            {currentCoctail.strIngredient1 && <p><b>1:</b> {currentCoctail.strIngredient1}</p>}
+            {currentCoctail.strIngredient2 && <p><b>2:</b> {currentCoctail.strIngredient2}</p>}
+            {currentCoctail.strIngredient3 && <p><b>3:</b> {currentCoctail.strIngredient3}</p>}
+            {currentCoctail.strIngredient4 && <p><b>4:</b> {currentCoctail.strIngredient4}</p>}
+            {currentCoctail.strIngredient5 && <p><b>5:</b> {currentCoctail.strIngredient5}</p>}
+            {currentCoctail.strIngredient6 && <p><b>6:</b> {currentCoctail.strIngredient6}</p>}
+            {currentCoctail.strIngredient7 && <p><b>7:</b> {currentCoctail.strIngredient7}</p>}
+            {currentCoctail.strIngredient8 && <p><b>8:</b> {currentCoctail.strIngredient8}</p>}
+            {currentCoctail.strIngredient9 && <p><b>9:</b> {currentCoctail.strIngredient9}</p>}
+            {currentCoctail.strIngredient10 && <p><b>10:</b> {currentCoctail.strIngredient10}</p>}
+          </div>
+        </section>
+        <hr />
+        <section className="coctail__description">
+          <h4>Instructions:</h4>
+          <p>{currentCoctail.strInstructions}</p>
+        </section>
+        <div className="modal__controls">
+          <Button onClick={() => switchCocktail('prev')}>Previous</Button>
+
+          <Button onClick={() => switchCocktail('next')}>Next</Button>
+        </div>
       </div>
 
     </div>
