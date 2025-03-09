d49a0eea353d7c83fb5769656926e482be43c7a1
ilya <mrsmile1988@gmail.com>
2025-03-09 13:30:02 +0300

    improved alcoFreeLabel animation

--- src/components/AlcoFreeLabel/AlcoFreeLabel.jsx
+++ src/components/AlcoFreeLabel/AlcoFreeLabel.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 import './alcoFreeLabel.scss'
 
-export default function AlcoFreeLabel () {
-  const [animation, setAnimation] = useState(null)
-  useEffect(()=> {
+export default function AlcoFreeLabel() {
+  const [animation, setAnimation] = useState(null);
+
+  useEffect(() => {
     setTimeout(() => {
-setAnimation('animation')
+      setAnimation('animation')
     }, 1000)
-  },[])
+  }, [])
+
   return (
     <div className={`cocktail-item__non-alco-label ${animation}`}></div>
   )
